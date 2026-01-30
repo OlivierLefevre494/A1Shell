84fe09d2700ca2a197c54502a6b798494effdf49
Hamza Rtel_Bennani <hamza.rbennani@gmail.com>
2026-01-29 20:54:33 -0500

    added chaining

--- starter-code/shell.c
+++ starter-code/shell.c
@@ -43,7 +43,8 @@ int main(int argc, char *argv[]) {
 int wordEnding(char c) {
     // You may want to add ';' to this at some point,
     // or you may want to find a different way to implement chains.
-    return c == '\0' || c == '\n' || c == ' ';
+
+    return c == '\0' || c == '\n' || c == ' ' || c == ';';
 }
 
 int parseInput(char inp[]) {
@@ -58,11 +59,37 @@ int parseInput(char inp[]) {
             tmp[wordlen] = inp[ix];                        
         }
         tmp[wordlen] = '\0';
-        words[w] = strdup(tmp);
-        w++;
-        if (inp[ix] == '\0') break;
-        ix++; 
+
+        if (wordlen > 0){
+            words[w++] = strdup(tmp);
+        }
+
+        if (inp[ix] == ';'){
+
+            if (w>0){
+                errorCode = interpreter(words, w);
+                if(errorCode == -1) return -1;
+
+                for (int i = 0; i < w; i++){
+                    free(words[i]);
+                }
+                w = 0;
+            }
+            ix++;
+            continue;
+        };
+
+        if (inp[ix] == '\0' || inp[ix] == '\n') 
+            break;
+        ix++;
     }
-    errorCode = interpreter(words, w);
+    if (w > 0){
+        errorCode = interpreter(words, w);
+
+        for (int i = 0; i < w; i++){
+            free(words[i]);
+        }
+    }
+
     return errorCode;
 }
